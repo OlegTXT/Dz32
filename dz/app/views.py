f053e7e0b53f2a28a196ac8732c476597852c167
USER1 <emoleg2008@gmail.com>
2024-03-12 21:43:12 +0500

    My Project

---
+++ dz/app/views.py
@@ -0,0 +1,25 @@
+from django.contrib import messages
+from django.core.signing import Signer
+from django.shortcuts import render, redirect
+from .models import Post
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def create_post(request):
+    if request.method == 'POST':
+        post = Post(title=request.POST['title'])
+        if post != '' and post != None:
+            message = messages.add_message(request, 100, f'" {post} " successfully created')
+            signer = Signer()
+            sing_post = signer.sign(post)
+            print(sing_post)
+            post.save()
+            return redirect('home')
+        else:
+            messages.add_message(request, 100,  f'" {post} " not created')
+            return redirect('home')
+
+    return render(request, 'create.html')
